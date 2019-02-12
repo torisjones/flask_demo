8dbf840b2cfb8f8d13152584b3ac4c15eb1d0703
Christopher Wiens <cwiens1@gmail.com>
2019-02-11 22:21:40 -0500

    Initial Commit

---
+++ HelloWorld/basic/server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import send_from_directory
+import os
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET"])
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route('/home', methods=["GET"])
+def serve_home_page():
+    return send_from_directory(app.config['STATIC_CONTENT_DIRECTORY'], "index.html")
+
+
+if __name__ == "__main__":
+    app.config['STATIC_CONTENT_DIRECTORY'] = os.path.dirname(os.path.abspath(__file__)) + "/static"
+    app.run()
