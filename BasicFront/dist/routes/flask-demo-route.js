e3b3a95683bbfde2ee97658086e4ffbd79c04eb6
Tori Jones <torijones@Toris-MacBook-Pro.local>
2019-02-13 22:55:33 -0600

    fix(*): laid out basic frontend, need to figure out what is wrong with post/put calls to finish

--- BasicFront/dist/routes/flask-demo-route.js
+++ BasicFront/dist/routes/flask-demo-route.js
@@ -1,12 +1,15 @@
+import {NessieApi} from "../utils/nessie-api";
+
 class FlaskDemoRoute extends HTMLElement {
   constructor(){
     super();
     this._shadowRoot = this.attachShadow({mode: 'open'});
+    this.api = new NessieApi();
   }
   connectedCallback(){
-    this._shadowRoot.innerHTML = `<style></style><h1>Nessie Flask Route!</h1><div id="pageDiv"><button id="newCustomer">New Customer</button></div>`;
-    this.createCustomerButton = this._shadowRoot.querySelector('#newCustomer');
+    this._shadowRoot.innerHTML = `<style></style><h1>Nessie Flask!</h1><div id="pageDiv"><button id="newCustomer">New Customer</button><br><button id="manageCustomers">Manage Customers</button></div>`;
     this.pageDiv = this._shadowRoot.querySelector('#pageDiv');
+    this.__getCustomers();
     this.__addEventListeners();
   }
   disconnectedCallback(){
@@ -19,10 +22,73 @@ class FlaskDemoRoute extends HTMLElement {
 
   }
   __addEventListeners(){
+    this.createCustomerButton = this._shadowRoot.querySelector('#newCustomer');
+    this.manageCustomersButton = this._shadowRoot.querySelector('#manageCustomers');
     this.createCustomerButton.addEventListener('click',this.__createCustomer.bind(this));
+    this.manageCustomersButton.addEventListener('click',this.__manageCustomers.bind(this));
   }
-  __createCustomer(){
-    this.pageDiv.innerHTML = '<nessie-flask-create-customer-page></nessie-flask-create-customer-page>';
+  loadHomePage(){
+    this.pageDiv.innerHTML = '<button id="newCustomer">New Customer</button>\n' +
+      '  <br>\n' +
+      '  <button id="manageCustomers">Manage Customers</button>';
+    this.__addEventListeners();
+  }
+  __getCustomers(){
+    this.api.getCall('/customers?key=6122e0b7dd9cf10ce7cb1135ac481e90').then((data)=>{
+      console.log('customers');
+      console.log(data);
+      this.customerList = data;
+    }).catch((error)=>{
+      console.log(error);
+    });
+  }
+  __createCustomer(customer){
+    this.pageDiv.innerHTML = '<create-customer></create-customer>';
+    if(customer.address){this.pageDiv.querySelector('create-customer').data = customer; }
+    this.pageDiv.querySelector('create-customer').addEventListener('back',this.loadHomePage.bind(this));
+    this.pageDiv.querySelector('create-customer').addEventListener('create-customer',this.createCustomerCall.bind(this));
+  }
+  createCustomerCall(event){
+    console.log('inside create customer on route');
+    console.log(event.detail);
+    // TODO: actually make call to flask app
+    if(event.detail._id){
+      //TODO make put call
+      console.log('inside make put call');
+      // this.api.putCall()
+    } else {
+      //TODO make post call
+      console.log('inside make post call');
+    }
+    this.loadHomePage();
+  }
+  __manageCustomers(){
+    this.pageDiv.innerHTML = '<manage-customers></manage-customers>';
+    this.pageDiv.querySelector('manage-customers').customers = this.customerList;
+    this.pageDiv.querySelector('manage-customers').addEventListener('back',this.loadHomePage.bind(this));
+    this.pageDiv.querySelector('manage-customers').addEventListener('manage-customers',this.manageCustomersCall.bind(this));
+  }
+  manageCustomersCall(event){
+    console.log('inside manage customer on route');
+    console.log(event.detail);
+    // TODO: actually make call to flask app
+    switch (event.detail.type) {
+      case 'edit':
+        this.__editCustomer(event.detail.id);
+        break;
+      case 'accounts':
+        break;
+    }
+    this.loadHomePage();
+  }
+  __editCustomer(id) {
+    this.api.getCall(`/customers/${id}?key=6122e0b7dd9cf10ce7cb1135ac481e90`).then((data)=>{
+      console.log('data');
+      console.log(data);
+      this.__createCustomer(data);
+    }).catch((error)=>{
+      console.log(error);
+    });
   }
 }
 if(!customElements.get('flask-demo-route')){
---
+++ BasicFront/src/pages/manage-customers/manage-customers.js
@@ -0,0 +1,67 @@
+class ManageCustomers extends HTMLElement {
+  constructor(){
+    super();
+    this._shadowRoot = this.attachShadow({mode: 'open'});
+  }
+  connectedCallback(){
+    this._shadowRoot.innerHTML = tempHtml;
+    this.backButton = this._shadowRoot.querySelector('#back');
+    this.deleteButton = this._shadowRoot.querySelector('#delete');
+    this.customerDiv = this._shadowRoot.querySelector('#customerDiv');
+    this.__addEventListeners();
+  }
+  disconnectedCallback(){
+
+  }
+  attributeChangedCallback(){
+
+  }
+  adoptedCallback(){
+
+  }
+  __addEventListeners(){
+    this.backButton.addEventListener('click',this.back.bind(this));
+  }
+  get customers(){
+    return this._customers;
+  }
+  set customers(_value){
+    this._customers = _value;
+    this.__buildCustomers();
+  }
+  __buildCustomers(){
+    this.customers.forEach((customer)=>{
+      let customerContainer = document.createElement('div');
+      customerContainer.setAttribute('class','container');
+      customerContainer.innerHTML = `<div class='name'>${customer.first_name} ${customer.last_name}</div>
+        <div class="addressLine1">${customer.address.street_number} ${customer.address.street_name}</div>
+        <div class="addressLine2">${customer.address.city}, ${customer.address.state} ${customer.address.zip}</div>
+        <button id="${customer._id}" class="edit-button">Edit Customer Details</button>`;
+      this.customerDiv.appendChild(customerContainer);
+    });
+    this.customerDiv.querySelectorAll('.edit-button').forEach((button)=>{
+      button.addEventListener('click',this.__editCustomer.bind(this));
+    });
+  }
+
+  back(){
+    console.log('back');
+    this.dispatchEvent(new CustomEvent('back'));
+  }
+  __deleteCustomer(event){
+    console.log('inside delete customer');
+    console.log(event.currentTarget.getAttribute('id'));
+  }
+  __editCustomer(event){
+    console.log('inside edit customer');
+    console.log(event.currentTarget.getAttribute('id'));
+    let id = event.currentTarget.getAttribute('id');
+    this.__emitUpdateEvent({type:'edit',id:id});
+  }
+  __emitUpdateEvent(event){
+    this.dispatchEvent(new CustomEvent('manage-customers',{detail:event}));
+  }
+}
+if(!customElements.get('manage-customers')){
+  customElements.define('manage-customers', ManageCustomers);
+}
